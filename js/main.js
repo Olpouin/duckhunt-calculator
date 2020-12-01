be0c37be856981fda77f849060f3a21736963014
Olpouin <olpouin@live.fr>
2020-12-01 18:59:02 +0100

    Page system

--- js/main.js
+++ js/main.js
@@ -6,12 +6,3 @@ header.onreadystatechange= function() {
 	document.getElementsByTagName('body')[0].insertAdjacentHTML("afterbegin",this.responseText); //Adding the header
 };
 header.send();
-
-var head = new XMLHttpRequest();
-head.open('GET', `content/head.html`, true);
-head.onreadystatechange= function() {
-	if (this.readyState!==4) return;
-	if (this.status!==200) return;
-	document.getElementsByTagName('head')[0].insertAdjacentHTML("beforeend",this.responseText); //Ading the general head elements
-};
-head.send();
